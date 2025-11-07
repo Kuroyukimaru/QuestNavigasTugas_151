3a148453d6e67be17d77aed782eb7fd782650cd9
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-07 16:11:34 +0700

    membuat fun HalamanListPeserta

--- app/src/main/java/com/example/questnavigastugas/view/Data.kt
+++ app/src/main/java/com/example/questnavigastugas/view/Data.kt
@@ -1,2 +1,20 @@
 package com.example.questnavigastugas.view
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanListPeserta(navController: NavHostController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFECD7FC)
+    ){
+
+    }
+}
