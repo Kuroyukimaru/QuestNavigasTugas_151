9e14b382b9ca019de0f5e5d9aa0b3cae2eb610d0
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-07 16:13:19 +0700

    didalam colum ada text List Daftar Peserta

--- app/src/main/java/com/example/questnavigastugas/view/Data.kt
+++ app/src/main/java/com/example/questnavigastugas/view/Data.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,7 +29,13 @@ fun HalamanListPeserta(navController: NavHostController) {
                 .padding(horizontal = 24.dp, vertical = 16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-
+            Text(
+                text = stringResource(R.string.listdaftarpeserta),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF42217E),
+                modifier = Modifier.padding(top = 16.dp, bottom = 20.dp)
+            )
         }
     }
 }
