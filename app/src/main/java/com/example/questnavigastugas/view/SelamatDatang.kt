d269a99509cf16c975cf56bb83744f08e93bd38b
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-07 15:31:34 +0700

    membuat column

--- app/src/main/java/com/example/questnavigastugas/view/SelamatDatang.kt
+++ app/src/main/java/com/example/questnavigastugas/view/SelamatDatang.kt
@@ -4,4 +4,13 @@ fun HalamanSelamatDatang(navController: NavHostController) {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFFECD7FC)
-    )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+    }
+}
