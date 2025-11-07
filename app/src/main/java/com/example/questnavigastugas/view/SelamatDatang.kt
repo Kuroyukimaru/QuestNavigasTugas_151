640d82c43394c1cab9b219a9b47e6c5011b95468
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-07 15:35:43 +0700

    di bawah selamat datang ada gambar logo gede dan tambahin gambarnya

--- app/src/main/java/com/example/questnavigastugas/view/SelamatDatang.kt
+++ app/src/main/java/com/example/questnavigastugas/view/SelamatDatang.kt
@@ -1,5 +1,26 @@
 package com.example.questnavigastugas.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.R
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
 fun HalamanSelamatDatang(navController: NavHostController) {
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -17,6 +38,14 @@ fun HalamanSelamatDatang(navController: NavHostController) {
                 fontSize = 25.sp,
                 color = Color(0xFF42217E),
                 fontWeight = FontWeight.Medium
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.unipath),
+                contentDescription = "Logo",
+                modifier = Modifier.size(600.dp)
             )
         }
     }
